251a4a02193981bef8a197b116fd24460cfbe92b
mihailgrachev04-create <mihailgrachev04@gmail.com>
2026-05-02 00:40:09 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const app = express();
 
-// Убираем X-Powered-By
 app.disable('x-powered-by');
 
-// Добавляем заголовки безопасности
 app.use((req, res, next) => {
   res.setHeader('X-Frame-Options', 'DENY');
   res.setHeader('X-Content-Type-Options', 'nosniff');
-  res.setHeader('Content-Security-Policy', "default-src 'self'");
+
+  
+  res.setHeader(
+    'Content-Security-Policy',
+    "default-src 'self'; script-src 'self'; style-src 'self'; object-src 'none'; frame-ancestors 'none'; base-uri 'self';"
+  );
+
+  
+  res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+
+  
+  res.setHeader('Cache-Control', 'no-store');
+
+  
   res.setHeader('Permissions-Policy', 'geolocation=()');
+
   next();
 });
 
