6150dc372bf0c8af94f78bef1fa2dd7b7bbaa6a6
mihailgrachev04-create <mihailgrachev04@gmail.com>
2026-05-02 00:34:34 +0300

    Update app.js
    first attempt to fix DAST

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const app = express();
 
+// Убираем X-Powered-By
+app.disable('x-powered-by');
+
+// Добавляем заголовки безопасности
+app.use((req, res, next) => {
+  res.setHeader('X-Frame-Options', 'DENY');
+  res.setHeader('X-Content-Type-Options', 'nosniff');
+  res.setHeader('Content-Security-Policy', "default-src 'self'");
+  res.setHeader('Permissions-Policy', 'geolocation=()');
+  next();
+});
+
 app.get('/', (req, res) => {
   res.send('Hello DevSecOps');
 });
